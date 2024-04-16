1d7c4a302654d1d8d4158f860f1546a615d8d72e
Ricardo Perez <rxperez12@gmail.com>
2024-04-16 16:43:21 -0700

    part1 - WIP

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Let's get this party started!");
+async function getGifSearch(evt) {
+  const searchTerm = document.querySelector("#search-term").value;
+  const searchParams = new URLSearchParams({
+    q: searchTerm, 'api_key': "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+  });
+  const gifData = await fetch(`http://api.giphy.com/v1/gifs/search?${searchParams}`);
+  console.log(gifData);
+}
+
+
+document.querySelector('#search-giphy').addEventListener('click', getGifSearch);
+
