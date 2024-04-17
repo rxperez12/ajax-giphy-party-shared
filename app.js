a31da2f16233ec8e23edb67a63771532b38af93d
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-16 17:08:29 -0700

    updated button type and added evt.preventDefault

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 async function getGifSearch(evt) {
+  evt.preventDefault();
+
   const searchTerm = document.querySelector("#search-term").value;
+  console.log(searchTerm);
   const searchParams = new URLSearchParams({
     q: searchTerm, 'api_key': "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
   });
-  const gifData = await fetch(`http://api.giphy.com/v1/gifs/search?${searchParams}`);
-  console.log(gifData);
+  const resp = await fetch(`http://api.giphy.com/v1/gifs/search?${searchParams}`);
+
+  const data = await resp.json();
+
+  console.log(
+    "getGifSearch resp=", resp, "data=", data);
+
+  //showHand(data.hand);
 }
 
 
 document.querySelector('#search-giphy').addEventListener('click', getGifSearch);
-
